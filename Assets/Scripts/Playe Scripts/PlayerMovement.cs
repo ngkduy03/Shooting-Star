e267322e37b57dc3135f7c643a643da8ce524c82
ngkduy03 <khangduyb4@gmail.com>
2023-01-16 00:16:04 +0700

    player ship movement - uncomplete

--- Assets/Scripts/Playe Scripts/PlayerMovement.cs
+++ Assets/Scripts/Playe Scripts/PlayerMovement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private float mouseSpeed = 0.0001f;
+    private void Start() {
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        gameObject.transform.position = mousePosition;
+        transform.Translate(Input.GetAxis("Horizontal")*mouseSpeed*Time.deltaTime,
+            Input.GetAxis("Vertical")*mouseSpeed*Time.deltaTime,0f); 
     }
 }
