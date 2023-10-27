9176a7df104a236ddb1fb6d73f3e5a6cd7a58497
Khang Dui <khangduyb4@gmail.com>
2023-10-27 15:02:30 +0700

    limit move of the ship depend on the size of the camera

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private void Start() {
-       Cursor.lockState = CursorLockMode.Locked; 
-       transform.position = new Vector2(0,-15); 
+    [SerializeField]
+    private Camera myCamera;
+
+    [SerializeField]
+    private PolygonCollider2D myCollider;
+
+    public Vector2 WorldUnitsInCamera;
+    private float cursorSpeed = 100f;
+    private float maxX;
+    private float minX;
+    private float maxY;
+    private float minY;
+
+    private void Start()
+    {
+        WorldUnitsInCamera.y = myCamera.orthographicSize;
+        WorldUnitsInCamera.x = WorldUnitsInCamera.y * Screen.width / Screen.height;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        transform.position = new Vector2(0, -15);
+
+        maxX = WorldUnitsInCamera.x - myCollider.bounds.size.x;
+        minX = -WorldUnitsInCamera.x + myCollider.bounds.size.x;
+        maxY = WorldUnitsInCamera.y - myCollider.bounds.size.y / 2;
+        minY = -WorldUnitsInCamera.y + myCollider.bounds.size.y;
     }
+
     void Update()
     {
-        Vector2 mousePosition = (Vector3)Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-        HandleMovement(99f);
+        HandleMovement(cursorSpeed);
     }
-    private void HandleMovement(float speed){
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x,-50f,50f),
-            Mathf.Clamp(transform.position.y,-27f,28f));
-        transform.Translate(Input.GetAxis("Mouse X")*speed*Time.deltaTime,
-            Input.GetAxis("Mouse Y")*speed*Time.deltaTime,0f);
+
+    private void HandleMovement(float speed)
+    {
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY)
+        );
+
+        transform.Translate(
+            Input.GetAxis("Mouse X") * speed * Time.deltaTime,
+            Input.GetAxis("Mouse Y") * speed * Time.deltaTime,
+            0f
+        );
     }
 }
